74b6bff957c102b4f75a695dfed525e3d1922a20
oHate <ohateign@gmail.com>
2021-12-05 23:59:00 -0600

    Remove Config and use System Properties

--- src/main/java/cc/minetale/quartz/QuartzServer.java
+++ src/main/java/cc/minetale/quartz/QuartzServer.java
@@ -1,41 +1,22 @@
 package cc.minetale.quartz;
 
-import cc.minetale.quartz.config.Config;
-import cc.minetale.quartz.config.ConfigLoader;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.extras.MojangAuth;
 import net.minestom.server.extras.velocity.VelocityProxy;
 
-import java.io.File;
-import java.io.IOException;
-
 public class QuartzServer {
 
     public static void main(String[] args) {
-        try {
-            File configFile = new File("config.json");
-            Config config = ConfigLoader.loadConfig(new Config(), configFile);
+        MinecraftServer minecraftServer = MinecraftServer.init();
 
-            if(config != null) {
-                MinecraftServer minecraftServer = MinecraftServer.init();
+        MinecraftServer.setBrandName("Quartz");
 
-                MinecraftServer.setBrandName("Quartz");
+        if (Boolean.getBoolean("velocityEnabled"))
+            VelocityProxy.enable(System.getProperty("velocitySecret", ""));
+        else
+            MojangAuth.init();
 
-                if (config.isVelocityEnabled())
-                    VelocityProxy.enable(config.getVelocitySecret());
-                else
-                    MojangAuth.init();
-
-                minecraftServer.start(config.getAddress(), config.getPort());
-            } else {
-                MinecraftServer.stopCleanly();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            MinecraftServer.stopCleanly();
-        }
+        minecraftServer.start(System.getProperty("bindAddress", "0.0.0.0"), Integer.getInteger("serverPort", 25565));
     }
 
 }
