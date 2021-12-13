709ec51a73ab12c5ae94f7d35ebb934179c8e761
oHate <ohateign@gmail.com>
2021-12-13 00:06:15 -0600

    Updated to 1.18.1

--- src/main/java/cc/minetale/quartz/generator/VoidGenerator.java
+++ src/main/java/cc/minetale/quartz/generator/VoidGenerator.java
@@ -1,23 +1,16 @@
 package cc.minetale.quartz.generator;
 
-import net.minestom.server.MinecraftServer;
 import net.minestom.server.instance.ChunkGenerator;
 import net.minestom.server.instance.ChunkPopulator;
 import net.minestom.server.instance.batch.ChunkBatch;
-import net.minestom.server.world.biomes.Biome;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class VoidGenerator implements ChunkGenerator {
 
     @Override
-    public void generateChunkData(ChunkBatch batch, int chunkX, int chunkZ) {}
-
-    @Override
-    public void fillBiomes(Biome[] biomes, int chunkX, int chunkZ) {
-        Arrays.fill(biomes, MinecraftServer.getBiomeManager().getById(0));
-    }
+    public void generateChunkData(@NotNull ChunkBatch batch, int chunkX, int chunkZ) {}
 
     @Override
     public List<ChunkPopulator> getPopulators() {
